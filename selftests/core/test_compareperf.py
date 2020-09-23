a26fb5db5477ff4783ec27e06a958ab66fd6d03e
Lukáš Doktor <ldoktor@redhat.com>
2020-09-23 11:43:20 +0200

    selftests: Fix xunit result check in compareperf test
    We used to compare our output to our output.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- selftests/core/test_compareperf.py
+++ selftests/core/test_compareperf.py
@@ -17,6 +17,7 @@ Tests for the main runperf app
 """
 
 import os
+import re
 from unittest import mock
 
 from runperf import ComparePerf
@@ -25,8 +26,10 @@ from . import Selftest
 
 
 class RunPerfTest(Selftest):
+    maxDiff = None
 
     def setUp(self):
+        super().setUp()
         self.base_dir = os.path.dirname(os.path.dirname(
             os.path.dirname(__file__)))
 
@@ -62,9 +65,13 @@ class RunPerfTest(Selftest):
                                "html_result.html")) as exp:
             with open(html_path) as act:
                 self.assertEqual(exp.read(), act.read())
-        with open(xunit_path) as exp:
+        with open(os.path.join(self.base_dir, "selftests", ".assets",
+                               "results", "result.xunit")) as exp:
             with open(xunit_path) as act:
-                self.assertEqual(exp.read(), act.read())
+                act_filt = re.sub('timestamp="[^"]+"',
+                                  'timestamp="2020-09-01T11:13:07.912376"',
+                                  act.read())
+                self.assertEqual(exp.read(), act_filt)
 
     def test(self):
         args = ["compare-perf", "--", "selftests/.assets/results/1_base/"
