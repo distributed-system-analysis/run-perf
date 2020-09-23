57d40acb43dac7b5d22730432c2be2f7ab80d9d0
Lukáš Doktor <ldoktor@redhat.com>
2020-09-23 09:31:20 +0200

    selftests: Add utils selftests
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- selftests/utils/test_utils.py
+++ selftests/utils/test_utils.py
@@ -1,10 +1,15 @@
+import collections
+import os
+import re
+import tempfile
+from unittest import mock
 import unittest
 
 from runperf import utils
-import collections
 
 
 class BasicUtils(unittest.TestCase):
+
     def test_list_of_threads(self):
         self.assertRaises(AssertionError, utils.list_of_threads, -5)
         self.assertRaises(AssertionError, utils.list_of_threads, 0)
@@ -14,6 +19,73 @@ class BasicUtils(unittest.TestCase):
         self.assertEqual(utils.list_of_threads(18), "1,4,8,12,16,18")
         self.assertEqual(utils.list_of_threads(79), "1,19,38,57,76,79")
         self.assertEqual(utils.list_of_threads(2048), "1,512,1024,1536,2048")
+
+    def test_read_write_file(self):
+        with tempfile.TemporaryDirectory(prefix="runperf-") as tmpdir:
+            path = os.path.join(tmpdir, "dir", "file")
+            self.assertEqual(-1, utils.read_file(path))
+            utils.write_file(path, "foo")
+            self.assertEqual("foo", utils.read_file(path))
+
+    def test_comma_separated_ranges_to_list(self):
+        text = ""
+        self.assertEqual([], utils.comma_separated_ranges_to_list(text))
+        text = "0,1-2,10,15-17"
+        self.assertEqual([0, 1, 2, 10, 15, 16, 17],
+                         utils.comma_separated_ranges_to_list(text))
+
+    def test_random_string(self):
+        out = utils.random_string(10)
+        self.assertEqual(10, len(out))
+
+    def test_check_output(self):
+        self.assertEqual("aaa\n", utils.check_output(["echo", "aaa"]))
+        self.assertEqual("aaa\n", utils.check_output("echo aaa", shell=True))
+        # read on closed stdin should return immediately (otherwise it hangs)
+        self.assertEqual("", utils.check_output(["cat", "/dev/stdin"]))
+
+    def test_wait_for(self):
+        with mock.patch("time.time", mock.Mock(side_effect=[0, 0, 0])):
+            func = mock.Mock(side_effect=[0, 1])
+            ret = utils.wait_for(func, 1)
+            self.assertEqual(True, ret)
+        with mock.patch("time.time", mock.Mock(side_effect=[0, 0, 1])):
+            ret = utils.wait_for(lambda: False, 1)
+            self.assertEqual(None, ret)
+
+    def test_tabular_output(self):
+        self.assertEqual("", utils.tabular_output([]))
+        self.assertEqual("b", utils.tabular_output([['b'], []]))
+        self.assertEqual('a   aa aaa\nbbb bb b',
+                         utils.tabular_output([["a", "aa", "aaa"],
+                                               ["bbb", "bb", "b"]]))
+        self.assertEqual('    HEADER\na   aa     aaa\nbbb bb',
+                         utils.tabular_output([["a", "aa", "aaa"],
+                                               ["bbb", "bb"]],
+                                              ["", "HEADER"]))
+
+    def test_string_to_safe_path(self):
+        stsp = utils.string_to_safe_path
+        self.assertEqual("", stsp(""))
+        self.assertEqual("_name", stsp(".name"))
+        self.assertEqual("a" * 255, stsp("a" * 300))
+        self.assertEqual("_n_a_m_e_", stsp(".n<a>m:e\""))
+        # each \u4500 turns into 3 characters
+        self.assertEqual('_' * 255, stsp("\u4500"*100))
+
+    def test_shell_write_content_cmd(self):
+        swcc = utils.shell_write_content_cmd
+        match = re.match(r'cat > \'\'"\'"\'"/path/to/file\' << \\([^\n]+)\n'
+                         r'some\nmultiline\ntext\n([^\n]+)',
+                         swcc("'\"/path/to/file", "some\nmultiline\ntext"))
+        self.assertNotEqual(match, None)
+        self.assertEqual(match[1], match[2])
+        match = re.match(r'cat >> \'\'"\'"\'"/path/to/file\' << \\([^\n]+)\n'
+                         r'some\nmultiline\ntext\n([^\n]+)',
+                         swcc("'\"/path/to/file", "some\nmultiline\ntext",
+                              True))
+        self.assertNotEqual(match, None)
+        self.assertEqual(match[1], match[2])
 
 
 if __name__ == '__main__':
