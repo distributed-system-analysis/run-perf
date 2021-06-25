278007de5ed7e5fe049ad0f0304200f20bab54a9
Lukáš Doktor <ldoktor@redhat.com>
2021-06-25 18:37:47 +0200

    Add "strip-run-perf" script
    This script can be used to create a stripped version of the run-perf
    results. It removes all the information which is not used by
    compare-perf like individual sample values or other assets, but for
    compare-perf (and other run-perf related scripts) it should preserve all
    of the values. The primary focus is to allow compare-perf/diff-perf/...
    execution on a minimal results when the full pbench data are stored
    somewhere else.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- selftests/core/test_compareperf.py
+++ selftests/core/test_compareperf.py
@@ -16,11 +16,13 @@
 Tests for the main runperf app
 """
 
+import glob
 import os
 import re
+import shutil
 from unittest import mock
 
-from runperf import ComparePerf
+from runperf import ComparePerf, StripPerf
 
 from . import Selftest
 
@@ -33,34 +35,35 @@ class RunPerfTest(Selftest):
         self.base_dir = os.path.dirname(os.path.dirname(
             os.path.dirname(__file__)))
 
-    def _run(self, args):
+    def _run(self, args, base_dir):
         old_path = os.getcwd()
         try:
-            os.chdir(self.base_dir)
+            os.chdir(base_dir)
             with mock.patch("sys.argv", args):
                 with mock.patch("logging.getLogger"):
                     return ComparePerf()()
         finally:
             os.chdir(old_path)
 
-    def test_full(self):
+    def test_full_and_stripped(self):
         html_path = os.path.join(self.tmpdir, "result.html")
         xunit_path = os.path.join(self.tmpdir, "result.xunit")
+        model_path = "selftests/.assets/results/1_base/linear_model.json"
+        results = ["selftests/.assets/results/1_base/"
+                   "result_20200726_080654",
+                   "selftests/.assets/results/1_base/result_20200726_112748",
+                   "selftests/.assets/results/2_kernel_update/"
+                   "result_20200726_114437",
+                   "selftests/.assets/results/3_kernel_and_less_cpus/"
+                   "result_20200726_125851", "selftests/.assets/results/"
+                   "4_kernel_and_less_cpus_and_different_duration/"
+                   "result_20200726_130256"]
         args = ["compare-perf", "--html-with-charts",
                 "--tolerance", "5", "--stddev-tolerance", "10",
-                "--model-linear-regression",
-                "selftests/.assets/results/1_base/linear_model.json",
+                "--model-linear-regression", model_path,
                 "--html", html_path, "--xunit", xunit_path,
-                "--", "selftests/.assets/results/1_base/"
-                "result_20200726_080654",
-                "selftests/.assets/results/1_base/result_20200726_112748",
-                "selftests/.assets/results/2_kernel_update/"
-                "result_20200726_114437",
-                "selftests/.assets/results/3_kernel_and_less_cpus/"
-                "result_20200726_125851", "selftests/.assets/results/"
-                "4_kernel_and_less_cpus_and_different_duration/"
-                "result_20200726_130256"]
-        self.assertEqual(self._run(args), 2)
+                "--"]
+        self.assertEqual(self._run(args + results, self.base_dir), 2)
         with open(os.path.join(self.base_dir, "docs", "source", "_static",
                                "html_result.html")) as exp:
             with open(html_path) as act:
@@ -73,13 +76,45 @@ class RunPerfTest(Selftest):
                                   act.read())
                 self.assertEqual(exp.read(), act_filt)
 
+        # Now try it again but using a stripped results
+        old_path = os.getcwd()
+        try:
+            os.chdir(self.base_dir)
+            for result in results:
+                with mock.patch("sys.argv",
+                                ["strip-perf", "-vvv", result,
+                                 os.path.join(self.tmpdir, result)]):
+                    with mock.patch("logging.getLogger"):
+                        StripPerf()()
+                # Make sure the stripped result is smaller than half of the
+                # original file size
+                for path in glob.glob(os.path.join(result, '*', '*', '*',
+                                                   'result.json')):
+                    exp = os.stat(path).st_size / 2
+                    act = os.stat(os.path.join(self.tmpdir, path)).st_size
+                    self.assertLess(act, exp)
+            shutil.copy(model_path, os.path.join(self.tmpdir, model_path))
+        finally:
+            os.chdir(old_path)
+        self.assertEqual(self._run(args + results, self.tmpdir), 2)
+        with open(os.path.join(self.base_dir, "docs", "source", "_static",
+                               "html_result.html")) as exp:
+            with open(html_path) as act:
+                self.assertEqual(exp.read(), act.read())
+        with open(os.path.join(self.base_dir, "selftests", ".assets",
+                               "results", "result.xunit")) as exp:
+            with open(xunit_path) as act:
+                act_filt = re.sub('timestamp="[^"]+"',
+                                  'timestamp="FILTERED"',
+                                  act.read())
+                self.assertEqual(exp.read(), act_filt)
 
     def test(self):
         args = ["compare-perf", "--", "selftests/.assets/results/1_base/"
                 "result_20200726_080654", "selftests/.assets/results/"
                 "4_kernel_and_less_cpus_and_different_duration/"
                 "result_20200726_130256"]
-        self.assertEqual(2, self._run(args))
+        self.assertEqual(2, self._run(args, self.base_dir))
 
     def test_paths(self):
         # with result name
@@ -87,16 +122,16 @@ class RunPerfTest(Selftest):
                 "result_20200726_080654", "selftests/.assets/results/"
                 "4_kernel_and_less_cpus_and_different_duration/"
                 "result_20200726_130256"]
-        self.assertEqual(2, self._run(args))
+        self.assertEqual(2, self._run(args, self.base_dir))
         # with incorrect path
         args = ["compare-perf", "--", "selftests/.assets/results/1_base/"
                 "result_20200726_080654",
                 os.path.join(self.tmpdir, "non", "existing", "location")]
         with mock.patch("sys.stderr"):
-            self.assertRaises(SystemExit, self._run, args)
+            self.assertRaises(SystemExit, self._run, args, self.base_dir)
         # with incorrect named path
         args = ["compare-perf", "--", "selftests/.assets/results/1_base/"
                 "result_20200726_080654", "foo:" +
                 os.path.join(self.tmpdir, "non", "existing", "location")]
         with mock.patch("sys.stderr"):
-            self.assertRaises(SystemExit, self._run, args)
+            self.assertRaises(SystemExit, self._run, args, self.base_dir)
